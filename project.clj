f62f3bee4ed6ef9ca6854ff3879d3d6803e4eb96
Sergio Alvarez-Napagao <salvarez@lsi.upc.edu>
2014-11-06 12:34:28 +0100

    Set up license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject com.github.tranchis/javaff "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU GPL v3"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main javaff.JavaFF
   :java-source-paths ["java"])
