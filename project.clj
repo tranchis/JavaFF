bea0b29216a04b122533f3828af3f3bc0b2bb54a
Sergio Alvarez-Napagao <salvarez@lsi.upc.edu>
2014-12-27 00:08:54 +0100

    Prepare clojars release.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.github.tranchis/javaff "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.github.tranchis/javaff "1.0.0"
+  :description "An implementation of FF Planner in Java."
+  :url "http://github.com/tranchis/javaff"
   :license {:name "GNU GPL v3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
